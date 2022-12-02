457bb92c94aac48e3a1455198b4c38bf68b79ef4
N0toriou5 <49403790+N0toriou5@users.noreply.github.com>
2022-12-02 15:35:04 +0100

    Update R_script.R

--- R_script.R
+++ R_script.R
@@ -17,3 +17,10 @@ library(stringr)
 require(vulcan)
 library(xlsx)
 register(SnowParam(12))
+dir.create("plots")
+dir.create("results")
+annotation <- read.table("rawdata/OID39197_Experimental_Report.txt",
+                         sep = "\t", as.is = TRUE, header = TRUE)
+# Load RMA normalized spot intensities
+raw <- read.table("rawdata/processed_data_files/Normalized_Calls_Files/100718_HG18_opt_expr_RMA.calls",
+                  sep = "\t", as.is = TRUE, header = TRUE)
